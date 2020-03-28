a3b5fd06c98750175b4c6c13114779827400eafc
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2020-03-29 01:13:57 +0400

    code cleanup

--- wayback-downloader.go
+++ wayback-downloader.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
-	"strings"
 	"wayback-downloader/wayback"
 )
 
@@ -32,50 +28,10 @@ func main() {
 	waybackURL, err := wayback.GetAnnualURL(domain, year)
 
 	// Request the HTML page.
-	res, err := http.Get(waybackURL)
+	content, err := wayback.GetHTML(waybackURL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
 
-	bodyBuf := bytes.NewBuffer([]byte{})
-	io.Copy(bodyBuf, res.Body)
-
-	lines := strings.Split(bodyBuf.String(), "\n")
-	removeLineStart := -1
-	removeLineEnd := -1
-	htmlStartIndex := -1
-	headRewriteIndex := -1
-	endOfHTML := -1
-
-	for i, line := range lines {
-		if strings.Contains(line, "<html") {
-			htmlStartIndex = i
-		}
-
-		if strings.Contains(line, "</html>") {
-			endOfHTML = i
-		}
-
-		if strings.Contains(line, "<!-- End Wayback Rewrite JS Include -->") {
-			headRewriteIndex = i
-		}
-
-		if strings.Contains(line, "<!-- BEGIN WAYBACK TOOLBAR INSERT -->") {
-			removeLineStart = i
-		}
-
-		if strings.Contains(line, "<!-- END WAYBACK TOOLBAR INSERT -->") {
-			removeLineEnd = i
-		}
-	}
-
-	lines[htmlStartIndex] = lines[htmlStartIndex] + "<head>"
-	tmpLine := append(lines[:htmlStartIndex+1], lines[headRewriteIndex+1:removeLineStart+1]...)
-
-	tmpLine = append(tmpLine, lines[removeLineEnd+1:endOfHTML+1]...)
-	ioutil.WriteFile("fb.html", []byte(strings.Join(tmpLine, "\n")), 0644)
+	ioutil.WriteFile("fb.html", []byte(content), 0644)
 }
---
+++ wayback/content.go
@@ -0,0 +1,63 @@
+package wayback
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// GetHTML handles url and after fetching HTML content tries to clean up
+func GetHTML(url string) (string, error) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("Unable to make an HTTP request -> %s", err.Error())
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	bodyBuf := bytes.NewBuffer([]byte{})
+	_, err = io.Copy(bodyBuf, res.Body)
+	if err != nil {
+		return "", fmt.Errorf("Unable fetch content from HTTP request -> %s", err.Error())
+	}
+
+	lines := strings.Split(bodyBuf.String(), "\n")
+	removeLineStart := -1
+	removeLineEnd := -1
+	htmlStartIndex := -1
+	headRewriteIndex := -1
+	endOfHTML := -1
+
+	for i, line := range lines {
+		if strings.Contains(line, "<html") {
+			htmlStartIndex = i
+		}
+
+		if strings.Contains(line, "</html>") {
+			endOfHTML = i
+		}
+
+		if strings.Contains(line, "<!-- End Wayback Rewrite JS Include -->") {
+			headRewriteIndex = i
+		}
+
+		if strings.Contains(line, "<!-- BEGIN WAYBACK TOOLBAR INSERT -->") {
+			removeLineStart = i
+		}
+
+		if strings.Contains(line, "<!-- END WAYBACK TOOLBAR INSERT -->") {
+			removeLineEnd = i
+		}
+	}
+
+	lines[htmlStartIndex] = lines[htmlStartIndex] + "<head>"
+	contentLines := append(lines[:htmlStartIndex+1], lines[headRewriteIndex+1:removeLineStart+1]...)
+	contentLines = append(contentLines, lines[removeLineEnd+1:endOfHTML+1]...)
+	return strings.Join(contentLines, "\n"), nil
+}
