e3e7750d933a3c754c8daaf2858bb5c742f357a7
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2020-03-29 13:22:35 +0400

    adding logging for 404

--- wayback/assets.go
+++ wayback/assets.go
@@ -103,7 +103,7 @@ func downloadAsset(url, outputDir string) string {
 
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Printf("ASSET FETCH: HTTP status error %d %s \n", res.StatusCode, res.Status)
+		log.Printf("ASSET FETCH: HTTP status error %d %s \n", res.Status, url)
 		return ""
 	}
 
